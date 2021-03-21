6fda6bebf4edb375d8a713da2f233e6c1a8044be
ETNJL <etnjl@outlook.com>
2021-03-21 22:50:15 +0800

    📃docs(Coding Block):
    Devide into 4 parts

--- helper.py
+++ helper.py
@@ -2,6 +2,7 @@ import os
 from os.path import join, getsize
 for root, dirs, files in os.walk('docs/Algorithm'):
     print(root,"\n")
+    files = sorted(files, key=lambda x: os.path.getctime((os.path.join(root, x))),reverse=True)
     for x in files:
         print("- "+x+": ",root.replace('\\','/').replace('docs/','')+'/'+x)
     print("\n")
